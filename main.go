b1701887db9728f00d1b15305fc104e59a6c6c7f
Leonardo-lucas-pereira <leonardo_lucasp@hotmail.com>
2022-07-31 22:55:54 -0300

    Função de Deposito e retorno multiplo.

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ type ContaCorrente struct {
 
 // Quando criamos uma função para a struct, temos que referenciar o obj -> (c *ContaCorrente)
 // Assim a função fica disponivel para o obj instanciado
-func (c *ContaCorrente) sacar(valorDoSaque float64) string {
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
 	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
 	if podeSacar {
 		c.saldo -= valorDoSaque
@@ -19,6 +19,16 @@ func (c *ContaCorrente) sacar(valorDoSaque float64) string {
 	} else {
 		return "Saldo insuficiente"
 	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Depósito realizado com sucesso", c.saldo
+	} else {
+		return "Valor inválido", c.saldo
+	}
+
 }
 
 func main() {
@@ -30,7 +40,10 @@ func main() {
 	conta.saldo = 20375.75
 
 	fmt.Println(conta.saldo)
-	fmt.Println(conta.sacar(60000))
+	fmt.Println(conta.Sacar(600))
 	fmt.Println(conta.saldo)
+	status, valor := conta.Depositar(6500)
+	fmt.Println(status)
+	fmt.Println("Saldo atual:", valor)
 
 }
