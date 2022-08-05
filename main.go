94b901f691a8ec960d0919e32a267ca3afe3a8e8
Leonardo-lucas-pereira <leonardo_lucasp@hotmail.com>
2022-08-04 21:27:53 -0300

    Visibilidade de atributos e metodos

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"Banco/clientes"
 	"Banco/contas"
 	"fmt"
 )
 
 func main() {
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(100)
 
-	cliente := clientes.Titular{Nome: "Leonardo", CPF: "123.456.895.56", Profissao: "Engenheiro"}
-	fmt.Println(cliente)
-	conta := contas.ContaCorrente{Titular: cliente, NumeroAgencia: 123, NumeroConta: 123456, Saldo: 89567.32}
-	fmt.Println(conta)
-
+	fmt.Println(contaExemplo)
+	fmt.Println(contaExemplo.ObterSaldo())
 }
