4760d117c6b8e0b23856692179e79f25d0b89435
Leonardo-lucas-pereira <leonardo_lucasp@hotmail.com>
2022-08-04 21:44:39 -0300

    Interface

--- main.go
+++ main.go
@@ -5,10 +5,20 @@ import (
 	"fmt"
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
-	contaPoupanca := contas.ContaPoupanca{}
-	contaPoupanca.Depositar(100)
-	contaPoupanca.Sacar(80)
-	fmt.Println(contaPoupanca)
-	fmt.Println(contaPoupanca.ObterSaldo())
+	conta1 := contas.ContaCorrente{}
+	conta1.Depositar(100)
+	conta1.Sacar(80)
+	fmt.Println(conta1)
+	fmt.Println(conta1.ObterSaldo())
+	PagarBoleto(&conta1, 10)
+	fmt.Println(conta1.ObterSaldo())
 }
